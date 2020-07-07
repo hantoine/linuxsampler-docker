b515cc3552281a0bfeb7302c193d0f75d53a55aa
Antoine Hébert <hantoine02@gmail.com>
2020-07-07 10:45:44 -0400

    Add an initial linux sampler channel at launch

--- linuxsampler/entrypoint.sh
+++ linuxsampler/entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-if [ "$1" = "connect" ] ; then
-	jack_connect LinuxSampler:0 system:playback_1
-	jack_connect LinuxSampler:1 system:playback_2
-	jack_connect system:midi_capture_1 LinuxSampler:midi_in_0
-	exit
-fi
-
 set -m
 
 # Start JackD
 /usr/bin/jackd -R -d net -a $DOCKERHOST -n linuxsampler -i 1 -l 1 &
 
 # Start LinuxSampelr
-linuxsampler
+linuxsampler &
+
+# Initialize Linux Sampler channels
+sleep 1
+cat initial_channels.lsp  | nc localhost 8888 &
+
+# Connect LinuxSampler to local JACK
+sleep 1
+jack_connect LinuxSampler:0 system:playback_1
+jack_connect LinuxSampler:1 system:playback_2
+jack_connect system:midi_capture_1 LinuxSampler:midi_in_0
+
+wait
